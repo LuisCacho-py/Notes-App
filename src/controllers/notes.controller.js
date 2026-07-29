52337a33eedd2c8b95590938da9b7a18403b9f08
LuisCacho-py <luisg.cacho@outlook.com>
2026-07-28 21:30:30 -0600

    Updating Notes

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -18,12 +18,16 @@ notesCtrl.renderNotes = async (req, res) => {
     res.render('notes/all-notes', {notes})
 }
  
-notesCtrl.renderEditForm = (req, res) => {
-    res.send('edit form')
+notesCtrl.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id).lean();
+    res.render('notes/edit-note', {note})
 }
 
-notesCtrl.updateNote = (req, res) => {
-    res.send('Update note')
+notesCtrl.updateNote = async (req, res) => {
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description});
+    console.log(req.body)
+    res.redirect('/notes')
 }
 
 notesCtrl.deleteNote = async (req, res) => {
